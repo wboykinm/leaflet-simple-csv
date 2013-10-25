184cd3af56801913475c6d239b551a765b844cf6
Bill Morris <wboykinm@geosprocket.com>
2013-10-25 10:21:36 -0400

    screw gitignore man.

---
+++ config.js
@@ -0,0 +1,9 @@
+var dataUrl = 'data/vt_geonames.csv';
+var maxZoom = 12;
+var fieldSeparator = ',';
+var baseUrl = 'http://{s}.tiles.mapbox.com/v3/landplanner.map-xswoybbb/{z}/{x}/{y}.jpg';
+var baseAttribution = 'Map data &copy;<a href="http://www.openstreetmap.org/" target="_blank"> OpenStreetMap</a> contributors';
+var subdomains = '1234';
+var clusterOptions = {showCoverageOnHover: false, maxClusterRadius: 50};
+var labelColumn = "GNAME";
+var opacity = 1.0;
