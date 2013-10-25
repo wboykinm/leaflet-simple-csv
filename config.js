b7c31914d419a6983151cf088e41640b6d48d654
Bill Morris <wboykinm@geosprocket.com>
2013-10-25 10:51:00 -0400

    try again

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-var dataUrl = 'data/data.csv';
+var dataUrl = 'data/vt_geonames_short.csv';
 var maxZoom = 12;
 var fieldSeparator = '|';
 var baseUrl = 'http://a.tiles.mapbox.com/v3/landplanner.map-xswoybbb/{z}/{x}/{y}.png';
 var baseAttribution = 'Map data &copy;<a href="http://www.openstreetmap.org/" target="_blank"> OpenStreetMap</a> contributors';
 var subdomains = '1234';
 var clusterOptions = {showCoverageOnHover: false, maxClusterRadius: 50};
-var labelColumn = "Name";
+var labelColumn = "GNAME";
 var opacity = 1.0;
